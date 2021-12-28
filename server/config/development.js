97cc7dc6df0e5320e9cea1998ee20e3c1abcb758
tsaqifrazin1 <tsaqifgojek@gmail.com>
2021-12-29 03:58:14 +0800

    Deploy to Heroku

---
+++ server/config/development.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // enabled logging for development
+  logging: true,
+  seed: true,
+  db: {
+    url: 'mongodb://127.0.0.1/nodeblog'
+  }
+};
---
+++ server/config/testing.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // disbable logging for testing
+  logging: false,
+  db: {
+    url: 'mongodb://127.0.0.1/nodeblog-test'
+  }
+};
